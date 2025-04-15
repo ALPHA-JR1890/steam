41fa24d44b971a793d16be6845d3810a8d6d2a3e
JR <blackerfamily1@outlook.com>
2025-04-15 16:35:36 -0700

    Update and rename ProjectV1 to ProjectV1.ino

---
+++ ProjectV1.ino
@@ -0,0 +1,32 @@
+// C++ code
+//
+int moisture = 0;
+
+int Motor = 0;
+
+void setup()
+{
+  pinMode(A0, OUTPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(A0, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  moisture = analogRead(A1);
+  digitalWrite(A0, LOW);
+  digitalWrite(A2, LOW);
+  if (moisture > 300) {
+    Serial.print(moisture);
+    Serial.println(" (Motor on)");
+    digitalWrite(A2, LOW);
+  } else {
+    digitalWrite(A2, HIGH);
+    Serial.print(moisture);
+    Serial.println(" (Motor off)");
+  }
+  delay(1000); // Wait for 1000 millisecond(s)
+}
